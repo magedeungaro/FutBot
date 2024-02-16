9eaffd2a8f8a6dfe284f7344b2ab52862bf0e296
magedeungaro <magedbgt@gmail.com>
2024-02-16 11:42:01 -0300

    chat gpt

---
+++ app/business/open_ai/chat.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module OpenAi
+  class Chat
+    GPT_MODEL = 'gpt-4-1106-preview'
+    TEMPERATURE = 0.7
+
+    attr_accessor :messages # apenas para teste
+    def initialize
+      @messages = []
+    end
+
+    def client
+      @client ||= OpenAI::Client.new do |f|
+        f.response :logger, Logger.new($stdout), bodies: true
+      end
+    end
+
+    def chat(query)
+      @messages << { role: :user, content: query }
+      response = chat_completion
+      message = response.dig(:choices, 0, :message)
+      content = message[:content]
+      @messages << { role: :assistant, content: }
+      @messages.last
+    end
+
+    def add_system_context(content)
+      @messages << {
+        role: :system,
+        content:
+      }
+    end
+
+    private
+
+    def chat_completion
+      response = client.chat(
+        parameters: {
+          model: GPT_MODEL,
+          messages: @messages,
+          temperature: TEMPERATURE
+        }
+      )
+
+      response.deep_symbolize_keys if response.is_a?(Hash)
+    end
+  end
+end
