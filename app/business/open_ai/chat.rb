d5122b271fdcf76d48d5480886c465d01afc3127
magedeungaro <magedbgt@gmail.com>
2024-02-17 00:53:20 -0300

    fix alucinação e erro de retorno

--- app/business/open_ai/chat.rb
+++ app/business/open_ai/chat.rb
@@ -4,7 +4,7 @@ module OpenAi
   class Chat
     GPT_MODEL = 'gpt-4-1106-preview'
     TEMPERATURE = 0.7
-
+    
     def initialize
       @messages = [] << Prompts::PERSONALITY
     end
@@ -14,40 +14,47 @@ module OpenAi
         f.response :logger, Logger.new($stdout), bodies: true
       end
     end
-
+    
     def chat(query)
       @messages << { role: :user, content: query }
       analyze
+      retry_analyze unless valid?
       @messages.last
     end
 
     private
-
+    
     def analyze
       response = chat_completion
       message = response.dig(:choices, 0, :message)
       content = message[:content]
       @messages << { role: :assistant, content: } if content.present?
-
       return unless function_call?(message)
-
+      
       @messages << { role: :assistant, content: message[:function_call].to_json }
       results = execute_function_call(message)
-
+      
       @messages << results
-
+      
       analyze
+    end
+    alias_method :retry_analyze, :analyze
+    
+    def valid?
+      !@messages.last[:content].match?('arguments')
     end
 
     def execute_function_call(message)
       function = message.dig(:function_call, :name).to_sym
       return "#{function} is not a valid function." unless ToolCalls.respond_to?(function)
-
-      ToolCalls.method(function).call
+      
+      args = JSON.parse(message.dig(:function_call, :arguments)).deep_symbolize_keys
+      results = ToolCalls.method(function).call(**args)
+      results.nil? ? "Function call #{function} failed." : results
     end
 
     def function_call?(message)
-      message.try(:[], :role) == 'assistant' && message.try(:[], :function_call)
+      message.try(:[], :role) == 'assistant' && message.try(:[], :function_call).present?
     end
 
     def chat_completion
--- app/business/open_ai/tool_calls.rb
+++ app/business/open_ai/tool_calls.rb
@@ -8,15 +8,29 @@ module OpenAi
         },
         {
           name: :handle_sign_up,
-          description: 'Use essa função para responder torcedores que estão buscando se tornarem sócio torcedor'
+          description: 'Use essa função para responder torcedores que estão buscando se tornar sócio torcedor/assinar o camisa7.'
         },
         {
           name: :get_social_media_mood_prompt,
-          description: 'Use essa função para entender melhor como lidar com o torcedor que quer cancelar o programa sócio torcedor.'
+          description: 'Use essa função para entender melhor como lidar com o torcedor que quer cancelar o programa sócio torcedor/camisa7.'
         },
         {
           name: :handle_randomness,
           description: 'Use essa função para lidar com o torcedor que sair do contexto do clube.'
+        },
+        {
+          name: :get_club_info,
+          description: 'Use essa função para saber sobre o clube, como link oficial e telefone do clube; link, endereço físico e suporte da loja. Não inclui suporte nem link do camisa7.',
+          parameters: {
+            type: :object,
+            properties: {
+              info_kind: {
+                type: :string,
+                description: "Retorne o parâmetro que representa o tipo da busca. Os parâmetros possíveis são: club_phone, store_link, website_link, store_customer_support, e store_address"
+              }
+            },
+            required: %w[info_kind]
+          }
         }
       ]
     end
@@ -32,7 +46,7 @@ module OpenAi
     def self.handle_sign_up
       {
         role: :function,
-        content: 'Cara, entre no link https://www.botafogo.com.br/inscricoes/',
+        content: 'Cara, entre no link https://camisa7.botafogo.com.br/',
         name: 'handle_sign_up'
       }
     end
@@ -51,6 +65,14 @@ module OpenAi
         role: :function,
         content: 'Cara, desculpa tá? Mas só consigo auxiliar sobre o Fogão e o Camisa 7.',
         name: 'handle_randomness'
+      }
+    end
+
+    def self.get_club_info(info_kind:)
+      {
+        role: :function,
+        content: Prompts.club_info[info_kind.to_sym],
+        name: 'get_club_info'
       }
     end
   end
