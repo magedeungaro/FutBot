2a5248bd72b8fd8f9161361309e3e424517623c7
magedeungaro <magedbgt@gmail.com>
2024-02-17 09:53:40 -0300

    comenta initializer

--- config/initializers/telegram_bot_initializer.rb
+++ config/initializers/telegram_bot_initializer.rb
@@ -1,7 +1,7 @@
 # config/initializers/bot_initializer.rb
-Rails.application.config.after_initialize do
-  Thread.new do
-    @telegram_bot = Integration::Bot.instance
-    @telegram_bot.run
-  end
-end
+# Rails.application.config.after_initialize do
+#   Thread.new do
+#     @telegram_bot = Integration::Bot.instance
+#     @telegram_bot.run
+#   end
+# end
